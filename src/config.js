d8e1fff729019fc76a534b7dfeb2a852c22c9d8a
mohdriyaz0807 <mohdriyaz0807@gmail.com>
2023-05-01 11:58:59 +0530

    moved config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 window.config = {
-  APP_URL: "/Users/juhomi/Projects/others/GitHub-clone-Hackathon",
+  APP_URL: "https://reverent-kepler-61ac7c.netlify.app",
   CLIENT_ID: "2a5d4ab56cd1f1869be8",
   CLIENT_SECRET: "0d6b65a584b808cc4c234e2846776fd234d02cbe"
 };
